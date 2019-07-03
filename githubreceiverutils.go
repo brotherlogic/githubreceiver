cbc1b26385d51a9c7e9b69aaf713b35d614ff3bb
Simon Tucker <simon@brotherlogic@gmail>
2019-07-03 11:05:17 -0700

    Fixed ref spec

--- githubreceiverutils.go
+++ githubreceiverutils.go
@@ -8,7 +8,7 @@ import (
 )
 
 func (s *Server) processPing(ctx context.Context, ping *pb.Ping) error {
-	if ping.Ref == "/refs/heads/master" {
+	if ping.Ref == "refs/heads/master" {
 		s.Log(fmt.Sprintf("Starting build for %v", ping.Repository.Name))
 		s.builder.build(ctx, ping.Repository.Name, ping.Repository.FullName)
 		return nil
