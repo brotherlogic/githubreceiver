d9af5b7f6c5869ef25a4d6a8bddde2f5cb65ffb0
Simon Tucker <brotherlogic@gmail.com>
2020-09-02 07:17:46 -0700

    Clear state

--- githubreceiver.go
+++ githubreceiver.go
@@ -234,13 +234,7 @@ func (s *Server) Mote(ctx context.Context, master bool) error {
 
 // GetState gets the state of the server
 func (s *Server) GetState() []*pbg.State {
-	return []*pbg.State{
-		&pbg.State{Key: "backends", Text: fmt.Sprintf("%v", s.backends)},
-		&pbg.State{Key: "pull_fails", Value: s.pullFails},
-		&pbg.State{Key: "pulls", Value: int64(len(s.pqueue))},
-		&pbg.State{Key: "web_hooks", Value: s.webhookcount},
-		&pbg.State{Key: "web_hook_fails", Value: s.webhookfail},
-	}
+	return []*pbg.State{}
 }
 
 var (
